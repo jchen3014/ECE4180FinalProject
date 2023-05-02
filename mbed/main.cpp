bcdd88d99e9b075a2ae79bcdb4e054ec1ac4e6b1
Connor Talley <ctalley7@gatech.edu>
2023-05-01 20:58:13 -0400

    updated files from in-person workday

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -1,62 +1,71 @@
-#include "Mutex.h"
-#include "cmsis_os.h"
-#include "Thread.h"
 #include "mbed.h"
-#include "rtos.h" //might need rtos though
+#include <string>
 
-Serial pc(USBTX, USBRX);
-RawSerial bt(p28, p27);
-RawSerial pi(p9, p10);
-
+RawSerial  pc(USBTX, USBRX);
+RawSerial  pi(p9, p10);
+RawSerial  bt(p28,p27);
 DigitalOut solenoid(p30);
+DigitalOut led1(LED1);
+DigitalOut led4(LED4);
 
 volatile bool unlock = false;
+string btstring = "";
+string pistring = "";
 
+string password = "cordicupdate";
 
-
-//might need to mutex these as they are both serial, but it might be fine because reading
-void bt_recv() {
+void bt_recv()
+{
     while(bt.readable()) {
         char btchar = bt.getc();
-        if (false) {
-            unlock = true;
+        if (btchar == '\n') {
+            if ((btstring.rfind(password, 0) == 0 && btstring.rfind("unlock") != -1) || btstring.rfind("1") != -1) {
+                unlock = true;
+                pi.puts("BT TRIG UNLOCK");
+            }
+            btstring = "";
+        } else {
+            btstring += btchar;
         }
     }
 }
 
-//need to come up with some sort of communication standard for Pi communication
-void pi_recv() {
-    while (pi.readable()) {
+void pi_recv()
+{
+    while(pi.readable()) {
         char pichar = pi.getc();
-        if (false) {
-            unlock = true;
+        if (pichar == '\n') {
+            pc.puts(pistring.c_str());
+            if ((pistring.rfind(password, 0) != -1 && pistring.rfind("unlock") != -1) || pistring.rfind("1") != -1) {
+                unlock = true;
+            }
+            pistring = "";
+        } else {
+            pistring += pichar;
         }
     }
 }
 
-void unlock() {
-    
-}
-
-int main() {
+int main()
+{
+    pi.baud(9600);
     bt.baud(9600);
+    pc.baud(9600);
+
+    pi.attach(&pi_recv, Serial::RxIrq);
     bt.attach(&bt_recv, Serial::RxIrq);
 
-    pi.baud(9600); //this might need to be different
-    pi.attach(&pi_recv, Serial::RxIrq);
-
-    solenoid = 0;
-
-    
     while(1) {
+        led1 = unlock;
         if (unlock) {
+            pc.printf("Unlocking...\n");
             solenoid = 1;
-            Thread::wait(5000);
+            wait(5);
             solenoid = 0;
             unlock = false;
-            Thread::wait(5000); //forced solenoid cooldown so it doesn't burn out
-        } else {
-            Thread::wait(500);
+            led1 = unlock;
+            wait(5);
         }
+        sleep();
     }
 }
